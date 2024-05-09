a0cecf1a236ea080a914494e1c77e144b8c3919b
SusanPoudel <111071491+SusanPoudel@users.noreply.github.com>
2024-05-09 17:48:36 +0545

    fix

--- BasicsofDart/removewhitespace.dart
+++ BasicsofDart/removewhitespace.dart
@@ -1,4 +1,4 @@
 main() {
-  String detail = "M name is Susan Poudel. I am 17 years old.";
+  String detail = "M name is Susan Poudel. I am 17 years old.I live in pokhara";
   print(detail.trim());
 }
